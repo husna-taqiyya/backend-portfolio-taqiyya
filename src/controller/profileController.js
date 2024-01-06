9d3e3eb920f4c22f31f607f1db72ec7fbcff15c1
taqiyyatussalamah <hafshoh.najah@gmail.com>
2024-01-06 08:48:07 +0700

    handle data dummy

--- src/controller/profileController.js
+++ src/controller/profileController.js
@@ -1,11 +1,37 @@
-const get = (req, res) => {
+import { Prisma } from '..application/prisma.js';
+
+
+// PATH: METHOD GET UNTUK MENGAMBIL DATA PROFILE
+const get = async (req, res, next) => {
+    try {
+        // CEK KE DATABASE
+        let profile = await Prisma.profile.findfirst();
+
+        // jika kosong => kirim data dummy
+        if (!profile) {
+            // buat data dummy disini
+            profile = "data dummy"
+        }
+
+        // jika ada isinya => kirim data asli
+        res.status(200).json({
+            message: "berhasil ambil data profile"
+        });
+
+    } catch (error) {
+        next(error);
+    }
+
+
+
+
     // ambil data dari database;
-    const data = {
-        firstname: "john",
-        lastname: "doe",
-        email: "johndoe@gmail.com",
-        age: 18
-    };
+    // const data = {
+    //     firstname: "john",
+    //     lastname: "doe",
+    //     email: "johndoe@gmail.com",
+    //     age: 18
+    // };
 
     res.status(200).json({
         messege: "berhasil ambil data profile",
